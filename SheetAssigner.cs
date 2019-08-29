2bd6ac5ff02351366972bde1c97094c8f72b7938
surrealism90 <huangji90@hotmail.com>
2019-08-29 16:07:58 +0800

    3d procedural gen
    need help on why 3d object isn't spawning as it should

---
+++ SheetAssigner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheetAssigner : MonoBehaviour {
+	[SerializeField]
+	Texture2D[] sheetsNormal;
+	[SerializeField]
+	GameObject RoomObj;
+	public Vector3 roomDimensions = new Vector3(60,0,60);
+	public Vector3 gutterSize = new Vector3(40,0,40);
+	public void Assign(Room[,] rooms){
+		foreach (Room room in rooms){
+			//skip point where there is no room
+			if (room == null){
+				continue;
+			}
+			//pick a random index for the array
+			int index = Mathf.RoundToInt(Random.value * (sheetsNormal.Length -1));
+			
+            //find position to place room
+			Vector3 pos = new Vector3(room.gridPos.x * (roomDimensions.x + gutterSize.x) / 2, 0, room.gridPos.y * (roomDimensions.y + gutterSize.y));
+			RoomInstance myRoom = Instantiate(RoomObj, pos, Quaternion.identity).GetComponent<RoomInstance>();
+			myRoom.Setup(sheetsNormal[index], room.gridPos, room.type, room.doorTop, room.doorBot, room.doorLeft, room.doorRight);
+		}
+	}
+}
